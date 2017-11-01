4ba2abf17316baec07ae448c340fcdb15292ecb0
jiuna <214471746@qq.com>
2017-11-01 19:04:01 +0800

    时间线效果

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,19 +4,70 @@
     <div class="ts" @click="showHint">show hint</div>
     <div class="ts" @click="showPop">show pop</div>
     <div class="ts" @click="login">立即登录</div>
-    <img src="../assets/logo.png" height="200" width="200" alt="">
+    <img src="../assets/logo.png" height="100" width="100" alt="">
+    <time-line :time-msg="timeArr"></time-line>
   </div>
 </template>
 
 <script>
+import timeLine from '@/components/timeLine'
 export default {
   name: 'hello',
+  components:{timeLine},
   mounted () {
   },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      hint:['我是一个hint','我是一个hint提示~~~~~','我是一个hint!我是一个hint!','hint','提示~~']
+      hint:['我是一个hint','我是一个hint提示~~~~~','我是一个hint!我是一个hint!','hint','提示~~'],
+      timeArr:[
+        {
+          type:'yy',
+          value:'2017',
+          msg:'第1条消息'
+        },
+        {
+          type:'mm',
+          value:'5',
+          msg:'第1条消息'
+        },{
+          type:'mm',
+          value:'4',
+          msg:'第2条消息我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!'
+        },{
+          type:'mm',
+          value:'3',
+          msg:'第3条消息'
+        },{
+          type:'mm',
+          value:'2',
+          msg:'第4条消息'
+        },{
+          type:'mm',
+          value:'1',
+          msg:'第5条消息'
+        },{
+          type:'yy',
+          value:'2016',
+          msg:'第1条消息'
+        },{
+          type:'mm',
+          value:'12',
+          msg:'第1条消息'
+        },{
+          type:'mm',
+          value:'11',
+          msg:'第2条消息我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!我是一个hint!'
+        },{
+          type:'mm',
+          value:'10',
+          msg:'第3条消息'
+        },{
+          type:'mm',
+          value:'1',
+          msg:'第4条消息'
+        }
+      ]
     }
   },
   methods:{
---
+++ src/components/timeLine.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="timeLine">
+    <div v-for="(item,index) in timeMsg" :key="index">
+      <div v-if="item.type==='yy'" class="year clearfix">
+        <div class="yearCon rel fl">
+          <span class="yearValue abs">{{item.value}}</span>
+          <span class="line abs"></span>
+          <i class="point abs"></i>
+        </div>
+      </div>
+      <div v-if="item.type==='mm'" class="month clearfix">
+        <div class="monthCon rel fr">
+          <span class="monthValue abs">{{item.value}}月</span>
+          <span class="monthMsg">{{item.msg}}</span>
+          <span class="line abs" v-bind:class="{'last':timeMsg.length-1 == index}"></span>
+          <i class="point abs"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'timeLine',
+  props:{
+    timeMsg:Array
+  },
+  mounted () {
+    console.log(this)
+  },
+  data () {
+    return {
+    }
+  },
+  methods:{}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scope>
+@import "../style/mixin.scss";
+.yearCon{
+  height: 40px;
+  width: 30%;
+  .point{
+    width: 10px;
+    height: 10px;
+    background: #f60;
+    right:-5px;
+    border-radius: 50%;
+  }
+  .yearValue{
+    width: 60px;
+    height: 25px;
+    right:20px;
+    top:-7px;
+    line-height: 25px;
+    display: inline-block;
+    background: #f60;
+    color: #fff;
+    border-radius: 5px;
+  }
+  .line{
+    width: 6px;
+    height: 100%;
+    background: #ccc;
+    right: -3px;
+    top:0;
+  }
+}
+.monthCon{
+  min-height: 30px;
+  padding-bottom: 10px;
+  width: 70%;
+  .point{
+    width: 10px;
+    height: 10px;
+    background: #aaa;
+    left:-5px;
+    top:2px;
+    border-radius: 50%;
+  }
+  .monthValue{
+    width: 50px;
+    text-align: right;
+    left: -60px;
+    top:0;
+  }
+  .monthMsg{
+    width: 100%;
+    display: inline-block;  
+    text-align: left;
+    padding:0 15px;
+    box-sizing:border-box;
+  }
+  .line{
+    width: 6px;
+    height: 100%;
+    background: #ccc;
+    left: -3px;
+    top:0;
+  }
+}
+.line.last{
+  height: 5px;
+}
+</style>
