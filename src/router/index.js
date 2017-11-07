0ad6096025faf6bec44d982b3ab6cfa66d3aa9a7
jiuna <214471746@qq.com>
2017-11-07 17:31:46 +0800

    路由跳转后回到头部

--- src/router/index.js
+++ src/router/index.js
@@ -7,43 +7,42 @@ import invite from '@/view/invite'
 Vue.use(Router)
 
 const router = new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: index,
-      meta:{
-        pageTitle:"立即抢购"
-      }
-    },
-    {
-      path: '/hello',
-      name: 'hello',
-      component: Hello,
-      meta:{
-        pageTitle:"hello",
-        keepAlive:true
-      }
-    },
-    {
-      path: '/account',
-      name: 'account',
-      component: account,
-      meta:{
-        pageTitle:"我的账户",
-        requireAuth:true
-      }
-    },
-    {
-      path: '/invite',
-      name: 'invite',
-      component: invite,
-      meta:{
-        pageTitle:"我的邀请",
-        requireAuth:true
-      }
-    }
-  ]
+    routes: [{
+            path: '/',
+            name: 'index',
+            component: index,
+            meta: {
+                pageTitle: "立即抢购"
+            }
+        },
+        {
+            path: '/hello',
+            name: 'hello',
+            component: Hello,
+            meta: {
+                pageTitle: "hello",
+                keepAlive: true
+            }
+        },
+        {
+            path: '/account',
+            name: 'account',
+            component: account,
+            meta: {
+                pageTitle: "我的账户",
+                requireAuth: true
+            }
+        },
+        {
+            path: '/invite',
+            name: 'invite',
+            component: invite,
+            meta: {
+                pageTitle: "我的邀请",
+                requireAuth: true
+            }
+        }
+    ]
 })
 
 /*router.beforeEach((to, from, next) => {
@@ -52,18 +51,22 @@ const router = new Router({
 })*/
 //判断是否需要登录权限 以及是否登录
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.pageTitle
-  if (to.matched.some(res => res.meta.requireAuth)) {// 判断是否需要登录权限
-    if (localStorage.getItem('username') === "true") {// 判断是否登录
-      next()
-    } else {// 没登录则跳转到登录界面
-      next({
-        path: '/hello',
-        query: {unlogin:"你没有登录"}
-      })
+
+    document.title = to.meta.pageTitle
+    if (to.matched.some(res => res.meta.requireAuth)) { // 判断是否需要登录权限
+        if (localStorage.getItem('username') === "true") { // 判断是否登录
+            next()
+        } else { // 没登录则跳转到登录界面
+            next({
+                path: '/hello',
+                query: { unlogin: "你没有登录" }
+            })
+        }
+    } else {
+        next()
     }
-  } else {
-    next()
-  }
 })
+router.afterEach((to, from, next) => {
+    window.scrollTo(0, 0);
+});
 export default router
